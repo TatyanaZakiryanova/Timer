668dc52a5d62e2d80470ea7c7ccdcdc041def2c6
TatyanaZakiryanova <elenastrell@yandex.ru>
2024-12-04 20:19:08 +0300

    created simple timer with button switching

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -1,5 +1,39 @@
+import { useEffect, useState } from 'react';
+
 const Timer = () => {
-  return <div>Timer</div>;
+  const [seconds, setSeconds] = useState<number>(0);
+  const [isRunning, setIsRunning] = useState<boolean>(false);
+  const [intervalId, setIntervalId] = useState<NodeJS.Timeout | null>(null);
+
+  useEffect(() => {
+    return () => {
+      if (intervalId) {
+        clearInterval(intervalId);
+      }
+    };
+  }, [intervalId]);
+
+  const toggleTimer = () => {
+    if (isRunning) {
+      if (intervalId) {
+        clearInterval(intervalId);
+        setIntervalId(null);
+      }
+    } else {
+      const id = setInterval(() => {
+        setSeconds((prev) => prev + 1);
+      }, 1000);
+      setIntervalId(id);
+    }
+    setIsRunning(!isRunning);
+  };
+
+  return (
+    <div>
+      <div>{seconds}</div>
+      <button onClick={toggleTimer}>{isRunning ? 'Pause' : 'Start'}</button>
+    </div>
+  );
 };
 
 export default Timer;
